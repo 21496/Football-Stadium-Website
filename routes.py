bce1e7a35603d8496485025e7e6b06ade217d75b
21496 <22425@burnside.school.nz>
2025-08-05 23:00:06 +1200

    Search bar

--- routes.py
+++ routes.py
@@ -16,20 +16,12 @@ def home():
     conn.close()
     return render_template('home.html', title='HOME', stadiums = stadiums)
 
-@app.route("/all_stadiums")
-def all_stadiums():
-    conn = sqlite3.connect("stadium.db")
-    cur = conn.cursor()
-    cur.execute("SELECT Id FROM Stadium")
-    stadiums = cur.fetchall()
-    conn.close()
-    return render_template("all_stadiums.html", stadiums = stadiums)
 
 @app.route("/football/<int:Id>")
 def stadiums(Id):
     conn = sqlite3.connect("stadium.db")
     cur = conn.cursor()
-    cur.execute("SELECT Id, name FROM Stadium")
+    cur.execute("SELECT Id, name, image_path FROM Stadium")
     stadiums = cur.fetchall()
     conn.close()
     return render_template("stadiums.html", stadiums = stadiums, Id = Id)
